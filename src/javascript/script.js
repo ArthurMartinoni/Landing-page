9d9b9705bc0d0b5813bca7046ccac30d72a3ed2f
Arthur O. Martinoni <arolimar06@gmail.com>
2025-12-18 18:14:47 -0300

    chore(header): ajustes visuais e correções iniciais
    Ajusta posicionamento e espaçamento do logo em index.html.
    Corrige responsividade e typos no CSS (header.css, home.css).
    Melhora estilos dos botões e contorno visual em styles.css.
    Corrige inicialização e toggle do menu mobile em script.js.
    Testado manualmente em desktop/mobile.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,3 +4,20 @@ $(document).ready(function () {
     $("#mobile_btn").find("i").toggleClass("fa-x");
   });
 });
+
+function createReview(grade, totalReviews) {
+  let starsHtml = " ";
+
+  for (let i = 0; i < notEqual; i++) {
+    starsHtml += '<i class="fa-solid fa-star"></i>';
+  }
+  const finalLayout = `
+<div class="course-reviews">
+${starsHtml}
+<span>(${totalReviews}+)</span>
+</div>
+`;
+  document.getElementById("container-courses").innerHTML += finalLayout;
+}
+
+createReview(5, 500);
